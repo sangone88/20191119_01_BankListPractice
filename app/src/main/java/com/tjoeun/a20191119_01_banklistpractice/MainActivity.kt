f9f0d05831d701038e43196d1f682c6a3920e8f0
sangone88 <llil1@naver.com>
2019-11-19 20:51:02 +0900

    리스트뷰 / 어댑터 연결  - 서버에서 데이터로드 완료되면 새로고침

--- app/src/main/java/com/tjoeun/a20191119_01_banklistpractice/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191119_01_banklistpractice/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.tjoeun.a20191119_01_banklistpractice.adapters.BankAdapter
 import com.tjoeun.a20191119_01_banklistpractice.datas.Bank
 import com.tjoeun.a20191119_01_banklistpractice.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 
 class MainActivity : BaseActivety() {
 
     var bankList = ArrayList<Bank>()
+    var bankAdapter:BankAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +27,12 @@ class MainActivity : BaseActivety() {
     }
 
     override fun setValuse() {
+
         getBanksFromServer()
+
+        bankAdapter = BankAdapter(mContext, bankList)
+        bankListView.adapter = bankAdapter
+
 
     }
 
@@ -38,15 +46,20 @@ class MainActivity : BaseActivety() {
                     val data = json.getJSONObject("data")
                     val banks = data.getJSONArray("banks")
 
-                    for (i in 0..banks.length()) {
+                    for (i in 0..banks.length()-1) {
                         val bankJSONObject = banks.getJSONObject(i)
                         val bankData = Bank.getBankFromJsonObject(bankJSONObject)
                         bankList.add(bankData)
                     }
+
+                    runOnUiThread {
+                        bankAdapter?.notifyDataSetChanged()
+                    }
+
                 }
                 else{
 //                    this@MainActivity -> mContext
-                    Toast.makeText(this@MainActivity, "서버 통신에 문제가 있습니다.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(mContext, "서버 통신에 문제가 있습니다.", Toast.LENGTH_SHORT).show()
                 }
             }
 
