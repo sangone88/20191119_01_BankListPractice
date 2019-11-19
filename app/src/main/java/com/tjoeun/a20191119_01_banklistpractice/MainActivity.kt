b3aa6847c69c0abf76040772c47f625e2f81ea15
sangone88 <llil1@naver.com>
2019-11-19 20:21:06 +0900

    API 호출 => 은행 목록 뽑아내기 직전

--- app/src/main/java/com/tjoeun/a20191119_01_banklistpractice/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191119_01_banklistpractice/MainActivity.kt
@@ -2,11 +2,16 @@ package com.tjoeun.a20191119_01_banklistpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.tjoeun.a20191119_01_banklistpractice.datas.Bank
+import com.tjoeun.a20191119_01_banklistpractice.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivety() {
 
     var bankList = ArrayList<Bank>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,6 +29,29 @@ class MainActivity : BaseActivety() {
     }
 
     fun getBanksFromServer() {
+        ServerUtil.getRequestBankList(mContext, object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(json: JSONObject) {
+                Log.d("응답확인", json.toString())
+                val code = json.getInt("code")
+
+                if (code == 200) {
+                    val data = json.getJSONObject("data")
+                    val banks = data.getJSONArray("banks")
+
+                    for (i in 0..banks.length()) {
+                        val bankJSONObject = banks.getJSONObject(i)
+
+
+                    }
+
+                }
+                else{
+//                    this@MainActivity -> mContext
+                    Toast.makeText(this@MainActivity, "서버 통신에 문제가 있습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        })
 
     }
 
--- app/src/main/java/com/tjoeun/a20191119_01_banklistpractice/datas/Bank.kt
+++ app/src/main/java/com/tjoeun/a20191119_01_banklistpractice/datas/Bank.kt
@@ -1,8 +1,21 @@
 package com.tjoeun.a20191119_01_banklistpractice.datas
 
+import org.json.JSONObject
+
 class Bank {
     var id = 0
     var code = ""
     var name = ""
     var loge= ""
+
+    companion object {
+        fun getBankFromJsonObject(json:JSONObject) : Bank {
+            val bank = Bank()
+
+//                파싱 코드들 작성
+
+            return bank
+
+        }
+    }
 }
