cef3c1a9dcb6f5e907dc0fb58139343c4b38bc97
sangone88 <llil1@naver.com>
2019-11-19 20:58:01 +0900

    상황에맞는 데이터 출력  - Glide 라이브러리를 이용해 이미지도 출력

--- app/src/main/java/com/tjoeun/a20191119_01_banklistpractice/adapters/BankAdapter.kt
+++ app/src/main/java/com/tjoeun/a20191119_01_banklistpractice/adapters/BankAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.tjoeun.a20191119_01_banklistpractice.R
 import com.tjoeun.a20191119_01_banklistpractice.datas.Bank
 
@@ -24,6 +27,14 @@ class BankAdapter(context: Context, res:Int, list:ArrayList<Bank>)
         }
 
         var row = tempRow!!
+
+        var logoImg = row.findViewById<ImageView>(R.id.logoImg)
+        var bankNameTxt = row.findViewById<TextView>(R.id.bankNameTxt)
+
+        var data = mList.get(position)
+
+        Glide.with(mContext).load(data.logo).into(logoImg)
+        bankNameTxt.text = data.name
 
         return row
     }
